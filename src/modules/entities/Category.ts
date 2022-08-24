9785753c95745717d4f1f7186b48e66e014f1b07
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-08-24 09:59:18 -0300

    Configuração do banco de dados

--- src/modules/entities/Category.ts
+++ src/modules/entities/Category.ts
@@ -1,9 +1,15 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("categories")
 class Category {
+    @PrimaryColumn()
     id?: string;
+    @Column()
     name!: string;
+    @Column()
     description!: string;
+    @Column()
     created_at!: Date;
 
     constructor() {
