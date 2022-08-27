b954591c06b9237dc713b0a4d44f661dac64c703
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-08-26 21:14:06 -0300

    Foi criada a documentação da API com o swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
 import express from "express";
 
+import "reflect-metadata";
+import { SwaggerUi } from "swagger-ui-express";
+
+import { router } from "./routes";
+import swaggerFile from "./swagger.json";
+
+import "./database";
+import "./shared/container";
+import { createConnection } from "typeorm";
+
+createConnection();
+
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
+app.use(router);
 
 app.get("/", (request, response) => {
     response.status(200).json({ message: "Server online" });
